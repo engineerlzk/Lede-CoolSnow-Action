eaa783ede9412bdfd0269104468889305f913a36
engineerlzk <engineerlzk@qq.com>
2020-08-27 16:50:07 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -5,12 +5,12 @@
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 #
 
-# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址,把下面的192.168.22.2修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.22.2/g' ./package/base-files/files/bin/config_generate
 
 
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
+# 修改主机名字，把Cool-Lede修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/Cool-Lede/g' ./package/base-files/files/bin/config_generate
 
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
